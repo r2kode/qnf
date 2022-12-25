6617a0cc1c601f50846fa71b9996dcb2e1f3c996
r2k <r2kodepl@gmailcom>
2022-12-25 14:05:13 +0100

    chore: cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
-
-export const yay = () => console.log('YIPPEE-KI-YAY');
-
-export const nay = () => console.log("It' alive!!!");
-
-export const nothig = () => console.log('publikuj te pierdoloną libkę!!!');
+export { relativeTime } from './datetime/relativeTime';
+export { removeDuplicates } from './utils/arrays.utils';
+export {
+  cloneObj,
+  cloneWithJson,
+  cloneWithGlobalMethod,
+} from './utils/objects.utils';
